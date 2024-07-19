c5aac01cfb0e36c2bc013f1059d3e852fb159bb7
KASolomon <kusiappiahsolomon10@gmail.com>
2024-07-19 14:07:37 +0000

    implement infinite queries for the /post endpoint

--- src/react-query/PostList.tsx
+++ src/react-query/PostList.tsx
@@ -1,21 +1,54 @@
-import usePosts from './hooks/usePosts';
-
-
+import { useState } from "react";
+import usePosts from "./hooks/usePosts";
+import React from "react";
 
 const PostList = () => {
-
-const {data : posts, error, isLoading} = usePosts()
+  const [userId, setUserId] = useState<number>();
+  const [page, setPage] = useState(1);
+const pageSize = 10
+  const { data, error, isLoading, fetchNextPage } = usePosts({userId, page, pageSize});
   if (error) return <p>{error.message}</p>;
   if (isLoading) return <p>Getting Posts...</p>;
 
+
   return (
-    <ul className="list-group">
-      {posts?.map((post) => (
-        <li key={post.id} className="list-group-item">
-          {post.title}
-        </li>
-      ))}
-    </ul>
+    <>
+      <select
+        className="mb-4 form-select"
+        name="users"
+        value={userId}
+        onChange={(event) => setUserId(parseInt(event.target.value))}
+      >
+        <option value=""></option>
+        <option value="1">User 1</option>
+        <option value="2">User 2</option>
+        <option value="3">User 3</option>
+      </select>
+      <ul className="list-group">
+        {data?.pages.map((page) => (
+          <React.Fragment>
+            {page.map((post) => (
+              <li key={post.id} className="list-group-item">
+                {post.title}
+              </li>
+            ))}
+          </React.Fragment>
+        ))}
+      </ul>
+      {/* <button
+        disabled={page == 1}
+        onClick={() => setPage(page - 1)}
+        className="btn btn-primary me-2 mt-2"
+      >
+        Previous
+      </button> */}
+      <button
+        onClick={()=>fetchNextPage()}
+        className="btn btn-primary me-2 mt-2"
+      >
+        Next
+      </button>
+    </>
   );
 };
 
--- src/react-query/hooks/usePosts.ts
+++ src/react-query/hooks/usePosts.ts
@@ -1,10 +1,44 @@
-import useData from "./useData";
+import {
+  InfiniteData,
+  QueryKey,
+  useInfiniteQuery,
+} from "@tanstack/react-query";
+import axios from "axios";
 interface Post {
   id: number;
   title: string;
   body: string;
   userId: number;
 }
-const usePosts = () => useData<Post>("/posts", ["posts"]);
 
+interface PostQuery {
+  userId: number | undefined;
+  page: number;
+  pageSize: number;
+}
+const usePosts = (queryObject: PostQuery) => {
+  return useInfiniteQuery<
+    Post[],
+    Error,
+    InfiniteData<Post[], number>,
+    QueryKey,
+    number
+  >({
+    initialPageParam: 1,
+    queryKey: ["posts"],
+    queryFn: async ({ pageParam = 1 }) => {
+      return axios
+        .get("https://jsonplaceholder.typicode.com/posts", {
+          params: {
+            _start: (pageParam - 1) * 10,
+            _limit: queryObject.pageSize,
+          },
+        })
+        .then((res) => res.data);
+    },
+    getNextPageParam: (lastPage, allPages) =>
+      lastPage.length > 0 ? allPages.length + 1 : undefined,
+  });
+};
+// "https://jsonplaceholder.typicode.com/posts";
 export default usePosts;
